f907a6920061df19eb1160f6300c2ef0e9706f47
eswdd <simon@exemel.co.uk>
2013-09-24 12:16:25 +0100

    Adding copyright info

--- src/main/java/com/betfair/services/commons/testutil/shunit/ShUnitWrapper.java
+++ src/main/java/com/betfair/services/commons/testutil/shunit/ShUnitWrapper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013, The Sporting Exchange Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.betfair.services.commons.testutil.shunit;
 
 import java.io.File;
@@ -6,7 +22,6 @@ import java.io.File;
  * A wrapper around shunit test scripts. shunit test scripts are self contained test suites, so any implementer of this
  * interface will represent a suite. This wrapper allows ease of integration into all places where junit works.
  * @see ShUnitRunner
- * @author Simon Matic Langford
  */
 public interface ShUnitWrapper {
     File getScript();
